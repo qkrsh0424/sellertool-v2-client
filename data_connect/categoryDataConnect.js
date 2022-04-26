398dfa23d53eae612a396bb5c67bf882ce355eb4
PSH <qkrsh0424@naver.com>
2022-04-26 21:00:05 +0900

    FEAT : 상품 관리 페이지 생성하고 기능 구현하자 카테고리 불러오기, 카테고리 선택, 카테고리 생성, 카테고리 수정 기능 완성

---
+++ data_connect/categoryDataConnect.js
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { axiosAuthInterceptor } from "./axiosInterceptors"
+
+const API_ADDRESS = process.env.NODE_ENV == 'development' ? process.env.development.apiAddress : process.env.production.apiAddress
+const SCP_API_ADDRESS = process.env.NODE_ENV == 'development' ? process.env.development.scpApiAddress : process.env.production.scpApiAddress
+
+const categoryDataConnect = () => {
+    return {
+        searchListByWorkspaceId: async function (workspaceId) {
+            return await axiosAuthInterceptor.get(`${API_ADDRESS}/api/v1/category/workspaces/${workspaceId}`, {
+                withCredentials: true,
+                xsrfCookieName: 'api_csrf',
+                xsrfHeaderName: 'X-XSRF-TOKEN'
+            })
+        },
+        createOne: async function (workspaceId, body) {
+            return await axiosAuthInterceptor.post(`${API_ADDRESS}/api/v1/category/workspaces/${workspaceId}`, body, {
+                withCredentials: true,
+                xsrfCookieName: 'api_csrf',
+                xsrfHeaderName: 'X-XSRF-TOKEN'
+            })
+        },
+        updateOne: async function(workspaceId, body){
+            return await axiosAuthInterceptor.put(`${API_ADDRESS}/api/v1/category/workspaces/${workspaceId}`, body, {
+                withCredentials: true,
+                xsrfCookieName: 'api_csrf',
+                xsrfHeaderName: 'X-XSRF-TOKEN'
+            })
+        }
+    }
+}
+
+export {
+    categoryDataConnect
+}

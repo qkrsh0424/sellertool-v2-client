782759c4fa4b087c62dedfe96375016859179838
PSH <qkrsh0424@naver.com>
2023-06-13 20:36:37 +0900

    Fix. CustomDoubleTouch 수정

--- components/double-touch/CustomDoubleTouch.js
+++ components/double-touch/CustomDoubleTouch.js
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-export default function CustomDoubleTouch({
-    type,
-    onDoubleTouch = () => { },
-    children,
-    ...props
-}) {
-    let firstPress = true;
-    let lastTime = new Date().getTime();
-    let timer = null;
-    let delay = 300;
+// export default function CustomDoubleTouch({
+//     type,
+//     onDoubleTouch = () => { },
+//     children,
+//     ...props
+// }) {
+//     let firstPress = true;
+//     let lastTime = new Date().getTime();
+//     let timer = null;
+//     let delay = 300;
+
+//     const doubleTouch = (e) => {
+//         let now = new Date().getTime();
+//         if (firstPress) {
+//             firstPress = false;
+//             timer = setTimeout(() => {
+//                 firstPress = true;
+//             }, delay);
+//             lastTime = now;
+//         } else {
+//             let delta = new Date().getTime() - lastTime < delay;
+//             if (delta) {
+//                 clearTimeout(timer);
+//                 firstPress = true;
+//                 onDoubleTouch(e);
+//             }
+//         }
+//     };
+
+//     return React.createElement(
+//         type,
+//         {
+//             onTouchStart: (e) => doubleTouch(e),
+//             ...props
+//         },
+//         children
+//     )
+// }
+
+function CustomDoubleTouch({ type, onDoubleTouch = () => { }, children, ...props }) {
+    const [firstPress, setFirstPress] = useState(true);
+    const lastTimeRef = useRef(new Date().getTime());
+    const timerRef = useRef(null);
+    const delay = 500;
 
     const doubleTouch = (e) => {
-        let now = new Date().getTime();
+        const now = new Date().getTime();
         if (firstPress) {
-            firstPress = false;
-            timer = setTimeout(() => {
-                firstPress = true;
+            setFirstPress(false);
+            timerRef.current = setTimeout(() => {
+                setFirstPress(true);
             }, delay);
-            lastTime = now;
+            lastTimeRef.current = now;
         } else {
-            let delta = new Date().getTime() - lastTime < delay;
+            const delta = now - lastTimeRef.current < delay;
             if (delta) {
-                clearTimeout(timer);
-                firstPress = true;
+                clearTimeout(timerRef.current);
+                setFirstPress(true);
                 onDoubleTouch(e);
             }
         }
@@ -33,8 +67,10 @@ export default function CustomDoubleTouch({
         type,
         {
             onTouchStart: (e) => doubleTouch(e),
-            ...props
+            ...props,
         },
         children
-    )
+    );
 }
+
+export default CustomDoubleTouch;

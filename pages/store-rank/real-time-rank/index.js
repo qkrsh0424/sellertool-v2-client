29f0227b6275b56698a2a0e5d4e8fcb27041d451
PSH <qkrsh0424@naver.com>
2023-06-28 17:10:39 +0900

    Feat. store-rank/real-time-rank 페이지 구성

---
+++ pages/store-rank/real-time-rank/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import FooterMain from "../../../views/footer/FooterMain";
+import PrimaryNavbarMainComponent from "../../../views/navbar/primary-navbar";
+import SecondaryNavbarMainComponent from "../../../views/navbar/secondary-navbar-v2";
+import MainComponent from "../../../views/store-rank/real-time-rank";
+
+const StoreRankRealTimeRankPage = () => {
+    return (
+        <>
+            <Head>
+                <title>스토어 랭킹 | 셀러툴</title>
+            </Head>
+            <PrimaryNavbarMainComponent />
+            <SecondaryNavbarMainComponent />
+            <MainComponent />
+            <FooterMain />
+        </>
+    );
+}
+export default StoreRankRealTimeRankPage;

bf27c03c96bca6c7c27ce63ce65e8a7df8a64658
fkakd1205 <fkakd0102@naver.com>
2023-07-05 14:50:37 +0900

    FEAT : 스토어 랭킹 navbar 컴포넌트 및 data connect 파일 생성 Layout, NavList.component, NavList.styled => 스토어 랭킹 navbar 관련 파일 추가 nRankRecordDataConnect : createOne, searchRecordList => nrank_record 단일 생성 및 유저가 생성한 내역 조회 nRankRecordDetailDataConnect : searchOne => nrank_record의 id값으로 nrank_record_detail 조회

---
+++ data_connect/nRankRecordDataConnect.js
@@ -0,0 +1,33 @@
+import { axiosAuthInterceptor } from "./axiosInterceptors"
+import withMainApiCsrfWrapper from "../utils/withMainApiCsrfWrapper";
+
+const API_ADDRESS = process.env.NODE_ENV == 'development' ? process.env.development.storeRankApiAddress : process.env.production.storeRankApiAddress
+
+const nRankRecordDataConnect = () => {
+    return {
+        createOne: async function (body, headers) {
+            return await withMainApiCsrfWrapper(
+                () => axiosAuthInterceptor.post(`${API_ADDRESS}/api/v1/nrank-records`, body, {
+                    headers,
+                    withCredentials: true,
+                    xsrfCookieName: 'x_api_csrf_token',
+                    xsrfHeaderName: 'X-XSRF-TOKEN'
+                })
+            )
+        },
+        searchRecordList: async function (headers) {
+            return await withMainApiCsrfWrapper(
+                () => axiosAuthInterceptor.get(`${API_ADDRESS}/api/v1/nrank-records`, {
+                    headers,
+                    withCredentials: true,
+                    xsrfCookieName: 'x_api_csrf_token',
+                    xsrfHeaderName: 'X-XSRF-TOKEN'
+                })
+            )
+        }
+    }
+}
+
+export {
+    nRankRecordDataConnect
+}
---
+++ data_connect/nRankRecordDetailDataConnect.js
@@ -0,0 +1,23 @@
+import { axiosAuthInterceptor } from "./axiosInterceptors"
+import withMainApiCsrfWrapper from "../utils/withMainApiCsrfWrapper";
+
+const API_ADDRESS = process.env.NODE_ENV == 'development' ? process.env.development.storeRankApiAddress : process.env.production.storeRankApiAddress
+
+const nRankRecordDetailDataConnect = () => {
+    return {
+        searchList: async function (body, headers) {
+            return await withMainApiCsrfWrapper(
+                () => axiosAuthInterceptor.get(`${API_ADDRESS}/api/v1/nrank-record-details/nrank-records/${body.recordId}`, {
+                    headers,
+                    withCredentials: true,
+                    xsrfCookieName: 'x_api_csrf_token',
+                    xsrfHeaderName: 'X-XSRF-TOKEN'
+                })
+            )
+        }
+    }
+}
+
+export {
+    nRankRecordDetailDataConnect
+}

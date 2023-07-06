fff421ee08200577c0be2b5c8547357804876347
PSH <qkrsh0424@naver.com>
2023-07-06 16:48:55 +0900

    Feat. 이용 가이드 링크 생성

--- views/home/service-list-v2/ServiceList.component.js
+++ views/home/service-list-v2/ServiceList.component.js
@@ -32,17 +32,19 @@ const ServiceListComponent = () => {
                             </CardWrapper>
                         </Link>
                     </CardContainer>
-                    {/* <CardContainer>
+                    <CardContainer>
                         <Link
-                            href={`/store-rank/real-time-rank`}
+                            // href={`/store-rank/real-time-rank`}
+                            href={`/`}
                             passHref
+                            disabled
                         >
-                            <CardWrapper>
-                                <CardTitle>스토어 랭킹</CardTitle>
-                                <CardDescription>스토어 랭킹 체험하기</CardDescription>
+                            <CardWrapper style={{ background: '#f0f0f0', cursor: 'not-allowed' }}>
+                                <CardTitle style={{ color: '#808080' }}>스토어 랭킹</CardTitle>
+                                <CardDescription style={{ color: '#808080' }}>오픈 준비중</CardDescription>
                             </CardWrapper>
                         </Link>
-                    </CardContainer> */}
+                    </CardContainer>
                     <CardContainer>
                         <Link
                             href={'/product/dashboard'}
